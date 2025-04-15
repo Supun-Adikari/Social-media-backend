b6b0dfff3c278148e0794ffdf4e8b779c3db6138
Supun Adikari <adikariamssh@gmail.com>
2025-04-15 12:07:41 +0530

    Deleting functionality added

--- main.py
+++ main.py
@@ -13,7 +13,14 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 my_post = [{"title": "title of post 1", "content": "content of post 1", "id": 1},{"title": "title of post 2", "content": "content of post 2", "id": 2}]
-   
+
+
+def find_index_post(id):
+    for i,p in enumerate(my_post):
+        if p['id'] == id:
+            return i
+        
+
 @app.get("/")
 def root():
     return {"message": "Hellogg World"}
@@ -21,6 +28,7 @@ def root():
 
 @app.get("/posts") 
 def get_posts():
+    print(my_post)
     return {"Data": my_post}
 
 
@@ -42,4 +50,15 @@ def get_post(id: int):
 
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} was not found")
    
-    return {"message": f"Post with {id} was not found"}
+
+@app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+
+    if(index == None):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} does not exist")
+
+    
+    my_post.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
